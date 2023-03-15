8d693d912b177caf287b62f8a43ce004ce158d3f
users <kalmatovazarina1@gmail.com>
2023-03-15 14:35:51 +0600

    zarina commit

---
+++ src/Zarina.java
@@ -0,0 +1,10 @@
+public class Zarina {
+    String name;
+    String surName ;
+    String patronymic ;
+    int age ;
+    String ilivein ;
+    String hobby ;
+    String kurs ;
+    int meheight;
+}
